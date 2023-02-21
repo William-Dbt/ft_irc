60db89c8eb9b5edf2fafdd87296a101ffa0c2404
William <william.debotte77@gmail.com>
2023-02-21 20:57:24 +0100

    save test

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -5,8 +5,10 @@
 #include <poll.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define TIMEOUT_LISTENING	500
+#define BUFFER_SIZE			4096
 
 int	main(int argc, char *argv[]) {
 	// TODO
@@ -19,65 +21,102 @@ int	main(int argc, char *argv[]) {
 	int	serverFd,
 		opt;
 
-	struct sockaddr_in	address;
 
 	serverFd = socket(AF_INET, SOCK_STREAM, 0);
 	if (serverFd < 0) {
 		std::cerr << "Socket creation error" << std::endl;
 		return 1;
 	}
+	// Set socket to non-blocking mode
+	if (setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+		std::cerr << "Set socket option error" << std::endl;
+		return 293235;
+	}
+	if (fcntl(serverFd, F_SETFL, O_NONBLOCK) < 0) {
+		std::cerr << "Fcntl error" << std::endl;
+		return 145;
+	}
+
+	struct sockaddr_in	address;
+
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(atoi(argv[1]));
 
-	// Set socket to non-blocking mode
-	opt = 1;
-	setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
-
 	// open socket
-	if (bind(serverFd, (struct sockaddr*)&address, sizeof(address)) < 0 ) {
+	if (bind(serverFd, (struct sockaddr*)&address, sizeof(address)) < 0) {
 		std::cerr << "Error: bind socket failed." << std::endl;
 		return 2;
 	}
 
+	if (listen(serverFd, address.sin_port) < 0) {
+		std::cerr << "Error: listen failed." << std::endl;
+		return 3;
+	}
+
 
 	int	i,
-		maxConnect,
-		nbDone,
-		resu,
-		newSocket;
+		maxConnect;
 
-	unsigned int		cliLenght;
 	struct pollfd		*polls = NULL;
-	struct sockaddr_in	newAddress;
+
+	maxConnect = 1000;
 
 	// Initialization of the pool of address that we will listen for
-	maxConnect = 5;
 	polls = new pollfd[maxConnect];
 	for (i = 0; i < maxConnect; i++) {
 		polls[i].fd = -1;
 		polls[i].events = POLLIN;
-		polls[i].revents = 0;
 	}
 
 	// Initialize the server
 	polls[0].fd = serverFd;
 
-	// Put the server on listening mode
-	if (listen(serverFd, maxConnect) < 0) {
-		std::cerr << "Error: listen failed." << std::endl;
-		return 3;
-	}
-
-	int	n,
+	int	newSocket;/* ,
+		bytes,
+		n,
 		found,
-		bytes;
+		nbDone,
+		resu; */
 
-	char	buffer[4096];
+	// char				buffer[4096];
+	socklen_t			cliLenght;
+	struct sockaddr_in	newAddress;
 
 	while (1) {
-		nbDone = 0;
-		resu = poll(polls, maxConnect, TIMEOUT_LISTENING);
+		if (poll(&polls[0], maxConnect, TIMEOUT_LISTENING) < 0)
+			continue ;
+
+		if (polls[0].revents == POLLIN) {
+			cliLenght = sizeof(newAddress);
+			newSocket = accept(serverFd, (struct sockaddr*)&newAddress, &cliLenght);
+			if (newSocket == -1)
+				continue ;
+
+			std::cout << "----- New client -----" << std::endl;
+			std::cout << "IP: " << inet_ntoa(newAddress.sin_addr) << std::endl;
+			std::cout << "fd: " << newSocket << std::endl;
+			std::cout << "---------------------------" << std::endl << std::endl;
+		}
+		else {
+			for (int i = 0; i < maxConnect; i++) {
+				if (polls[i].revents == POLLIN) {
+					char	buffer[BUFFER_SIZE + 1];
+					ssize_t	size;
+
+					size = recv(polls[i].fd, &buffer, BUFFER_SIZE, 0);
+					if (size == -1)
+						continue ;
+
+					buffer[size] = '\0';
+
+					std::cout << buffer << std::endl;
+				}
+			}
+		}
+
+		/* nbDone = 0;
+		resu = poll(&polls[0], maxConnect, TIMEOUT_LISTENING);
 		// Check if there is something to read
 		if (resu < 0) {
 			sleep(5);
@@ -88,6 +127,8 @@ int	main(int argc, char *argv[]) {
 			nbDone++;
 			cliLenght = sizeof(newAddress);
 			newSocket = accept(serverFd, (struct sockaddr*)&newAddress, &cliLenght);
+			if (newSocket < 0)
+				continue ;
 
 			n = 0;
 			found = 0;
@@ -96,7 +137,7 @@ int	main(int argc, char *argv[]) {
 					found = 1;
 					polls[n].fd = newSocket;
 					std::cout << "----- New client -----" << std::endl;
-					bytes = read(polls[n].fd, buffer, 4096);
+					bytes = recv(polls[n].fd, buffer, 4096, MSG_PEEK);
 					buffer[bytes - 2] = '\0';
 					std::cout << buffer << std::endl;
 					std::cout << "---------------------------" << std::endl << std::endl;
@@ -106,22 +147,18 @@ int	main(int argc, char *argv[]) {
 			}
 
 			// Something for the clients?
-			// TODO blocking here
 			i = 1;
-			std::cout << nbDone << " " << resu << std::endl;
 			while (nbDone <= resu && i < maxConnect) {
-				std::cout << i << std::endl;
 				// If there is a client, check if he send something
 				if (polls[i].fd != -1 && polls[i].revents & POLLIN) {
-					std::cout << "fds: " << polls[i].fd << std::endl;
 					nbDone++;
-					bytes = read(polls[i].fd, buffer, 4096);
+					bytes = recv(polls[i].fd, buffer, 4096, MSG_PEEK);
 					buffer[bytes - 2] = '\0';
 					std::cout << "- " << buffer << std::endl;
 				}
 				i++;
 			}
-		}
+		} */
 	}
 	return 0;
 }
