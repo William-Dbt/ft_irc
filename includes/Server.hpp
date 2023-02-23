49e9bc333540d95d104b939ff0253517d5112ead
XavierLB <xavier.lebaux@gmail.com>
2023-02-23 18:39:35 +0100

    testing socket functions

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -1,7 +1,17 @@
 #include <sys/socket.h> // for socket(), bind(), listen(), accept()
 #include <sys/types.h>	// for socket types (SOCK_STREAM, SOCK_DGRAM)
 #include <netinet/in.h> // for sockaddr_in and inet_addr()
-#include <iostream>
+#include <iostream> 
+#include <cerrno> // for errno
+#include <cstring> // for strerror()
+#include <cstdlib> // for exit()
+#include <fcntl.h> // for fcntl()
+#include <poll.h> // for poll()
+#include <unistd.h> // for close()
+#include <arpa/inet.h> // for inet_ntoa()
+
+#define TIMEOUT_LISTENING	500
+#define BUFFER_SIZE			4096
 
 class Server
 {
@@ -10,4 +20,5 @@ public:
 	Server();
 	~Server();
 	void _init(int port);
+	void _acceptUser(int server_fd);
 };
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,69 +6,97 @@
 // #include <arpa/inet.h>
 #include "Server.hpp"
 
-#define PORT 8080
+// #define PORT 8080
+
+// int main(int argc, char *argv[])
+// {
+// 	(void)argc;
+// 	(void)argv;
+
+// 	Server server;
+
+// 	server._init(PORT);
+
+
+
+// 	// while (true)
+// 	// {
+// 	// 	int server_fd;
+// 	// 	int new_socket;
+// 	// 	int valread;
+// 	// 	struct sockaddr_in address;
+// 	// 	int opt = 1;
+// 	// 	socklen_t addrlen = sizeof(address);
+// 	// 	char buffer[1024] = {0};
+// 	// 	const char *hello = "Hello from server";
+
+// 	// 	// Creating socket file descriptor
+// 	// 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+// 	// 	{
+// 	// 		std::cerr << "socket failed" << std::endl;
+// 	// 		return (1);
+// 	// 	}
+
+// 	// 	// Forcefully attaching socket to the port 8080
+// 	// 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+// 	// 	{
+// 	// 		std::cerr << "setsockopt failed" << std::endl;
+// 	// 		return (1);
+// 	// 	}
+// 	// 	address.sin_family = AF_INET;
+// 	// 	address.sin_addr.s_addr = INADDR_ANY;
+// 	// 	address.sin_port = htons(PORT);
+
+// 	// 	// Forcefully attaching socket to the port 8080
+// 	// 	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+// 	// 	{
+// 	// 		std::cerr << "bind failed" << std::endl;
+// 	// 		return (1);
+// 	// 	}
+// 	// 	if (listen(server_fd, 3) < 0)
+// 	// 	{
+// 	// 		std::cerr << "listen failed" << std::endl;
+// 	// 		return (1);
+// 	// 	}
+// 	// 	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0)
+// 	// 	{
+// 	// 		std::cerr << "accept failed" << std::endl;
+// 	// 		return (1);
+// 	// 	}
+// 	// 	valread = read(new_socket, buffer, 1024);
+// 	// 	std::cout << buffer << std::endl;
+// 	// 	send(new_socket, hello, strlen(hello), 0);
+// 	// 	std::cout << "Hello message sent" << std::endl;
+
+// 	// 	// closing the connected socket
+// 	// 	close(new_socket);
+// 	// 	// closing the listening socket
+// 	// 	shutdown(server_fd, SHUT_RDWR);
+// 	// }
+// 	return 0;
+// }
+
 
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
-
 	Server server;
+	
+	if (argc != 2)
+	{
+		std::cout << "Usage: ./ircserv <port> <password>" << std::endl;
+		return 1;
+	}
 
-	server._init(PORT);
-	// while (true)
-	// {
-	// 	int server_fd;
-	// 	int new_socket;
-	// 	int valread;
-	// 	struct sockaddr_in address;
-	// 	int opt = 1;
-	// 	socklen_t addrlen = sizeof(address);
-	// 	char buffer[1024] = {0};
-	// 	const char *hello = "Hello from server";
-
-	// 	// Creating socket file descriptor
-	// 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-	// 	{
-	// 		std::cerr << "socket failed" << std::endl;
-	// 		return (1);
-	// 	}
-
-	// 	// Forcefully attaching socket to the port 8080
-	// 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-	// 	{
-	// 		std::cerr << "setsockopt failed" << std::endl;
-	// 		return (1);
-	// 	}
-	// 	address.sin_family = AF_INET;
-	// 	address.sin_addr.s_addr = INADDR_ANY;
-	// 	address.sin_port = htons(PORT);
-
-	// 	// Forcefully attaching socket to the port 8080
-	// 	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
-	// 	{
-	// 		std::cerr << "bind failed" << std::endl;
-	// 		return (1);
-	// 	}
-	// 	if (listen(server_fd, 3) < 0)
-	// 	{
-	// 		std::cerr << "listen failed" << std::endl;
-	// 		return (1);
-	// 	}
-	// 	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0)
-	// 	{
-	// 		std::cerr << "accept failed" << std::endl;
-	// 		return (1);
-	// 	}
-	// 	valread = read(new_socket, buffer, 1024);
-	// 	std::cout << buffer << std::endl;
-	// 	send(new_socket, hello, strlen(hello), 0);
-	// 	std::cout << "Hello message sent" << std::endl;
-
-	// 	// closing the connected socket
-	// 	close(new_socket);
-	// 	// closing the listening socket
-	// 	shutdown(server_fd, SHUT_RDWR);
-	// }
+	try
+	{
+		server._init(atoi(argv[1]));
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return (1);
+	}
+	
 	return 0;
 }
+
