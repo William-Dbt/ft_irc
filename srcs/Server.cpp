8048e4ab78be6e073638970a69cbeb115fcbed4c
Nicolas MATTERA <nmattera@e2r8p17.clusters.42paris.fr>
2023-03-22 18:55:13 +0100

    comment ->server.init()

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -16,8 +16,9 @@ Server::Server(const int &port, const std::string &password) : _fd(-1),
 															   _password(password),
 															   _nbClients(0) {}
 
-Server::~Server() {
-	std::map<int, Client*>::iterator	it;
+Server::~Server()
+{
+	std::map<int, Client *>::iterator it;
 
 	for (it = this->_clients.begin(); it != this->_clients.end(); it++)
 		delete (*it).second;
@@ -26,57 +27,83 @@ Server::~Server() {
 		close(this->_fd);
 }
 
-int	Server::initError(const int &exit_code, const std::string &error) {
+int Server::initError(const int &exit_code, const std::string &error)
+{
 	printServerLog(error);
 	return exit_code;
 }
 
-int	Server::init() {
+/* 
+ * init() function:
+ * 1. Create a new socket
+ * 2. Set socket options
+ * 3. Set the socket to non-blocking mode
+ * 4. Assign address for the socket
+ * 5. Listen to the given port
+*/
+
+int Server::init()
+{
 	int socketFd = -1;
 	int opt = 1;
 
-	struct sockaddr_in	address;
+	struct sockaddr_in address;
 
+	// init server socket
+	// -> AF_INET: IPv4
+	// -> SOCK_STREAM: TCP
 	socketFd = socket(AF_INET, SOCK_STREAM, 0);
 	if (socketFd == -1)
 		return initError(1, "Can't open new socket.");
 
-	#ifdef __linux__
-		if (setsockopt(socketFd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) == -1)
-			return initError(2, "Can't set option(s) to server socket.");
-	#else
-		if (setsockopt(socketFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
-			return initError(2, "Can't set option(s) to server socket.");
-	#endif
+// set socket options
+// -> SOL_SOCKET: socket level
+// [ allow several clients on the same time ]
+// -> SO_REUSEADDR: allow reuse of local addresses 
+// -> SO_REUSEPORT: allow reuse of local ports
+#ifdef __linux__
+	if (setsockopt(socketFd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) == -1)
+		return initError(2, "Can't set option(s) to server socket.");
+#else
+	if (setsockopt(socketFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
+		return initError(2, "Can't set option(s) to server socket.");
+#endif
 
+	// fcntl use to modify the file descriptor properties
+	// -> F_SETFL: set the file descriptor flags
+	// -> O_NONBLOCK: set the file descriptor to non-blocking mode
 	if (fcntl(socketFd, F_SETFL, O_NONBLOCK) == -1)
 		return (initError(3, "Can't set the socket to O_NONBLOCK."));
 
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(this->_port);
-	if (bind(socketFd, (struct sockaddr*)&address, sizeof(address)) == -1)
+
+	// bind the socket to the given port and address to listen to it
+	if (bind(socketFd, (struct sockaddr *)&address, sizeof(address)) == -1)
 		return (initError(4, "Can't assign address for the socket (using bind function)."));
 
+	// listen to the given port and address
 	if (listen(socketFd, address.sin_port) == -1)
 		return (initError(5, "Can't listen to the given port."));
 
 	this->_fd = socketFd;
-	this->_pfds.push_back(pollfd());
-	this->_pfds.back().fd = socketFd;
-	this->_pfds.back().events = POLLIN;
+	this->_pfds.push_back(pollfd()); // add the server socket to the pollfd vector
+	this->_pfds.back().fd = socketFd; // set the server socket fd
+	this->_pfds.back().events = POLLIN; // set the server socket to listen to POLLIN events
 	return 0;
 }
 
-void	Server::acceptClient() {
-	int					fd;
-	socklen_t			cliLenght;
-	struct sockaddr_in	address;
+void Server::acceptClient()
+{
+	int fd;
+	socklen_t cliLenght;
+	struct sockaddr_in address;
 
 	cliLenght = sizeof(address);
-	fd = accept(this->_fd, (struct sockaddr*)&address, &cliLenght);
+	fd = accept(this->_fd, (struct sockaddr *)&address, &cliLenght);
 	if (fd == -1)
-		return ;
+		return;
 
 	if (fcntl(fd, F_SETFL, O_NONBLOCK) == -1)
 		initError(1, "Can't set a client's fd to O_NONBLOCK.");
@@ -87,30 +114,36 @@ void	Server::acceptClient() {
 	this->_clients[fd] = new Client(fd, inet_ntoa(address.sin_addr), this);
 	printServerLog(fd, "New connection has been registered (fd: " + intToString(fd) + ").\n");
 	this->_nbClients++;
-	if (this->_nbClients > atoi(this->getConfig().get("max_users").c_str())) {
+	if (this->_nbClients > atoi(this->getConfig().get("max_users").c_str()))
+	{
 		printServerLog("Too many clients has been registered on the server. (maximum of " + this->getConfig().get("max_users") + " users are allowed)");
 		this->_clients[fd]->setQuitMessage("Server is full.");
 		this->_clients[fd]->status = DISCONNECTED;
 	}
 }
 
-void	Server::run() {
-	static int	lastPing = std::time(NULL);
+void Server::run()
+{
+	static int lastPing = std::time(NULL);
 
-	std::vector<pollfd>::iterator	it;
+	std::vector<pollfd>::iterator it;
 
 	if (poll(&this->_pfds[0], this->_pfds.size(), atoi(this->_config.get("ping").c_str()) * 1000) == -1)
-		return ;
+		return;
 
-	if (std::time(NULL) - lastPing >= atoi(this->_config.get("ping_delay").c_str())) {
+	if (std::time(NULL) - lastPing >= atoi(this->_config.get("ping_delay").c_str()))
+	{
 		sendPings();
 		lastPing = std::time(NULL);
 	}
-	else {
+	else
+	{
 		if (this->_pfds[0].revents == POLLIN)
 			this->acceptClient();
-		else {
-			for (it = this->_pfds.begin(); it != this->_pfds.end(); it++) {
+		else
+		{
+			for (it = this->_pfds.begin(); it != this->_pfds.end(); it++)
+			{
 				if ((*it).revents == POLLIN)
 					receiveEntries(it);
 			}
@@ -119,25 +152,28 @@ void	Server::run() {
 	deleteClients();
 }
 
-void	Server::receiveEntries(std::vector<pollfd>::iterator& it) {
-	char		readBuffer[4096];
-	int			bytes;
-	Client*		user = this->_clients[(*it).fd];
+void Server::receiveEntries(std::vector<pollfd>::iterator &it)
+{
+	char readBuffer[4096];
+	int bytes;
+	Client *user = this->_clients[(*it).fd];
 
 	bytes = recv((*it).fd, readBuffer, 4096, 0);
 	readBuffer[bytes] = '\0';
 	printServerLog((*it).fd, readBuffer, true);
-	if (bytes == 0) {
+	if (bytes == 0)
+	{
 		user->status = DISCONNECTED;
-		return ;
+		return;
 	}
 
-	size_t		pos = 0;
-	size_t		lastPos;
-	std::string	entryBuffer = readBuffer;
-	std::string	commandBuffer;
+	size_t pos = 0;
+	size_t lastPos;
+	std::string entryBuffer = readBuffer;
+	std::string commandBuffer;
 
-	while (pos != entryBuffer.size()) {
+	while (pos != entryBuffer.size())
+	{
 		lastPos = entryBuffer.find("\r\n", pos) + 2;
 		if (lastPos - 2 == std::string::npos)
 			lastPos = entryBuffer.find("\n", pos) + 1;
@@ -145,15 +181,17 @@ void	Server::receiveEntries(std::vector<pollfd>::iterator& it) {
 		commandBuffer = entryBuffer.substr(pos, lastPos - pos);
 		pos = lastPos;
 		if (commandBuffer.find("CAP LS") != std::string::npos)
-			continue ;
+			continue;
 
-		Command	command(this->_clients[(*it).fd], commandBuffer);
+		Command command(this->_clients[(*it).fd], commandBuffer);
 		command.execute();
 	}
-	if (user->status == FULLYREGISTER) {
-		if (user->getNickname().empty()) {
+	if (user->status == FULLYREGISTER)
+	{
+		if (user->getNickname().empty())
+		{
 			user->status = DISCONNECTED;
-			return ;
+			return;
 		}
 		printServerLog(user->getNickname() + "(" + intToString(user->getFd()) + ")" + " has been connected.");
 		user->status = CONNECTED;
@@ -161,31 +199,36 @@ void	Server::receiveEntries(std::vector<pollfd>::iterator& it) {
 	}
 }
 
-static void	deleteClientPollFd(std::vector<pollfd>& pfds, int fd) {
-	std::vector<pollfd>::iterator	it;
+static void deleteClientPollFd(std::vector<pollfd> &pfds, int fd)
+{
+	std::vector<pollfd>::iterator it;
 
-	for (it = pfds.begin(); it != pfds.end(); it++) {
-		if ((*it).fd == fd) {
+	for (it = pfds.begin(); it != pfds.end(); it++)
+	{
+		if ((*it).fd == fd)
+		{
 			pfds.erase(it);
-			break ;
+			break;
 		}
 	}
 }
 
-void	Server::deleteClients() {
-	std::map<int, Client*>::iterator	it;
+void Server::deleteClients()
+{
+	std::map<int, Client *>::iterator it;
 
-	std::vector<Client*>				usersToDelete;
-	std::vector<Client*>::iterator		deleteIt;
+	std::vector<Client *> usersToDelete;
+	std::vector<Client *>::iterator deleteIt;
 
 	for (it = this->_clients.begin(); it != this->_clients.end(); it++)
 		if ((*it).second->status == DISCONNECTED)
 			usersToDelete.push_back((*it).second);
 
 	if (!usersToDelete.size())
-		return ;
+		return;
 
-	for (deleteIt = usersToDelete.begin(); deleteIt != usersToDelete.end(); deleteIt++) {
+	for (deleteIt = usersToDelete.begin(); deleteIt != usersToDelete.end(); deleteIt++)
+	{
 		(*deleteIt)->sendTo("QUIT :" + (*deleteIt)->getQuitMessage());
 		printServerLog((*deleteIt)->getNickname() + "(" + intToString((*deleteIt)->getFd()) + ")" + " has been disconnected. (" + (*deleteIt)->getQuitMessage() + ")");
 		deleteClientPollFd(this->_pfds, (*deleteIt)->getFd());
@@ -197,14 +240,16 @@ void	Server::deleteClients() {
 	}
 }
 
-void	Server::sendPings() {
-	int	timeout = atoi(this->_config.get("timeout").c_str());
+void Server::sendPings()
+{
+	int timeout = atoi(this->_config.get("timeout").c_str());
 
-	std::map<int, Client*>::iterator	it;
+	std::map<int, Client *>::iterator it;
 
-	for (it = this->_clients.begin(); it != this->_clients.end(); it++) {
+	for (it = this->_clients.begin(); it != this->_clients.end(); it++)
+	{
 		if ((*it).second->status != CONNECTED)
-			continue ;
+			continue;
 
 		if (std::time(NULL) - (*it).second->getLastPing() >= timeout)
 			(*it).second->status = DISCONNECTED;
@@ -213,32 +258,34 @@ void	Server::sendPings() {
 	}
 
 	// TMP
-	std::map<std::string, Channel*>::iterator	chanIt;
-	std::map<int, Client*>::iterator		clientIt;
-	for (chanIt = this->_channels.begin(); chanIt != this->_channels.end(); chanIt++) {
+	std::map<std::string, Channel *>::iterator chanIt;
+	std::map<int, Client *>::iterator clientIt;
+	for (chanIt = this->_channels.begin(); chanIt != this->_channels.end(); chanIt++)
+	{
 		std::cout << KGRN << "Channel " << KBOLD << (*chanIt).first << KRESET << std::endl;
-		std::cout << KGRN << "key: " << KBOLD <<  (*chanIt).second->getKey() << KRESET << std::endl;
+		std::cout << KGRN << "key: " << KBOLD << (*chanIt).second->getKey() << KRESET << std::endl;
 
-		std::map<int, Client*>					clientInChannel = (*chanIt).second->getClients();
-		std::map<int, Client*>::iterator		clientInChannelIt;
+		std::map<int, Client *> clientInChannel = (*chanIt).second->getClients();
+		std::map<int, Client *>::iterator clientInChannelIt;
 
-		for (clientInChannelIt = clientInChannel.begin(); clientInChannelIt != clientInChannel.end(); clientInChannelIt++) {
+		for (clientInChannelIt = clientInChannel.begin(); clientInChannelIt != clientInChannel.end(); clientInChannelIt++)
+		{
 			std::cout << KGRN << "- " << (*clientInChannelIt).second->getNickname() << KRESET << std::endl;
 		}
 		std::cout << std::endl;
 	}
-	for (clientIt = this->_clients.begin(); clientIt != this->_clients.end(); clientIt++) 
+	for (clientIt = this->_clients.begin(); clientIt != this->_clients.end(); clientIt++)
 		std::cout << KYEL << "Client " << (*clientIt).second->getNickname() << KRESET << std::endl;
 }
 
-void	Server::addChannel(Channel *channel)
+void Server::addChannel(Channel *channel)
 {
 	this->_channels[channel->getName()] = channel;
 }
 
-
-Channel*	Server::getChannel(std::string name) {
-	std::map<std::string, Channel*>::iterator	it;
+Channel *Server::getChannel(std::string name)
+{
+	std::map<std::string, Channel *>::iterator it;
 
 	it = this->_channels.find(name);
 	if (it == this->_channels.end())
@@ -246,25 +293,29 @@ Channel*	Server::getChannel(std::string name) {
 	return (*it).second;
 }
 
-
-int	Server::getSocketFd() const {
+int Server::getSocketFd() const
+{
 	return this->_fd;
 }
 
-int	Server::getPort() const {
+int Server::getPort() const
+{
 	return this->_port;
 }
 
-std::string&	Server::getPassword() {
+std::string &Server::getPassword()
+{
 	return this->_password;
 }
 
-Config&	Server::getConfig() {
+Config &Server::getConfig()
+{
 	return this->_config;
 }
 
-Client*	Server::getClient(std::string nickname) {
-	std::map<int, Client*>::iterator	it;
+Client *Server::getClient(std::string nickname)
+{
+	std::map<int, Client *>::iterator it;
 
 	for (it = this->_clients.begin(); it != this->_clients.end(); it++)
 		if ((*it).second->getNickname() == nickname)
@@ -273,6 +324,7 @@ Client*	Server::getClient(std::string nickname) {
 	return NULL;
 }
 
-std::map<int, Client*>&	Server::getClients() {
+std::map<int, Client *> &Server::getClients()
+{
 	return this->_clients;
 }
